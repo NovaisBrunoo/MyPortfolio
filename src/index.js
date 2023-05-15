f1c29aa3877899eb173f761f4020587bb9d180e3
Bruno Novais <novais-bruno@hotmail.com>
2023-05-14 23:25:15 -0300

    Analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './style/global.css';
 import { BrowserRouter } from 'react-router-dom';
 import MainRoutes from '../src/routes/routes'
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <MainRoutes />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
